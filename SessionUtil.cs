b798f63aec0fa1f9cf069185b9301e75504ab3d5
SapinSobre <nat.dao3@gmail.com>
2019-05-28 13:41:55 +0200

    Add files via upload

---
+++ SessionUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ogre
+{
+    public static class SessionUtil
+    {
+        public static string ConnectionString {
+            get { return @"Server=(localdb)\MSSQLLocalDB;Database=LOgreEnSoucoupe;Trusted_Connection=true;"; }
+        }
+    }
+}
